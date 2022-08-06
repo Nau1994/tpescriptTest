7e0ec23890db4de2bbf5c9c7214d0bc4cd783b46
Naushad <naushad.a1013@gmail.com>
2022-08-06 05:32:08 +0530

    Promise

---
+++ src/Promise/Login.ts
@@ -0,0 +1,47 @@
+type user={
+    id1234:string,
+    id5678:string,
+    }
+
+export class Login{
+     
+    private  pass:user={
+    id1234:'1234',
+    id5678:'5678',
+    }
+    private name:user={
+    id1234:'name1234',
+    id5678:'name5678',
+    }
+
+    constructor(private id:string,private password:string){}
+
+    authenticate():Promise<boolean>{
+        return new Promise((resolve,reject)=>{
+            if( this.password===this.pass[this.id as keyof user]){
+                resolve(true);
+            }else{
+                // console.log('rejected');
+                reject(false)
+            }
+
+        })
+    }
+
+    userName(authenticate:boolean):Promise<string>{
+        return new Promise((resolve,reject)=>{
+            if( authenticate===true){
+                resolve(`Logged as ${this.name[this.id as keyof user]} !`);
+            }else{
+                // console.log('rejected');
+                reject('Authentication failed !')
+            }
+
+        })
+    }
+}
+
+// console.log((new Login('id1235','1234')).authenticate());
+// console.log((new Login('id1234','1234')).userName(true));
+
+
--- src/Promise/index.ts
+++ src/Promise/index.ts
@@ -1,1 +1,49 @@
-console.log('test 1');
+import { Login } from "./Login";
+
+let login = new Login('id1234','1234');
+// let login = new Login('id1234','1235');
+let res:string='';
+
+console.log("Before Promise"+res)
+
+login.authenticate()
+  .then(
+    //resolve()
+    function (value) {
+    //   console.log(value);
+    //   console.log("Success in Resolve()");
+      return login.userName(value);
+    }
+    ,
+    //Reject()
+    function (value) {
+    //   console.log(value);
+    //   console.log("Reject in Catch in Reject()");
+    return login.userName(value);
+    }
+  )
+  .catch(function (value) {
+    // console.log(value);
+    // console.log("Reject in Catch");
+    return login.userName(value);
+  })
+  .then(
+    //chained
+    function (value) {
+        res=value;
+        console.log("Inside then Promise "+res);
+      console.log(value);
+    //   console.log("Success in chained Resolve()");
+    }
+  )
+  .catch(function (value) {
+    res=value;
+    console.log("Inside catch Promise "+res);
+    console.log(value);
+    // console.log("Reject in Catch");
+  })
+  .finally(function () {
+    console.log("finally ");
+  });
+
+  console.log("After Promise"+res)
